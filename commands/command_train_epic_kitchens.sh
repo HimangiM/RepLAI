bee9a1c48e5db430aa2c6bfd9799ba74fbcd9681
Himangi Mittal <himangimittal@himangis-mbp.myfiosgateway.com>
2022-07-25 22:17:51 -0400

    train eval

--- commands/command_train_epic_kitchens.sh
+++ commands/command_train_epic_kitchens.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PYTHONPATH=. python main.py --config-name=default_ssrl -m \
-logging.name=epic_git2 \
+logging.name=log_train_epickitchens \
 data=epic_10s_audiopeak \
 backbone/video=avid_r2plus1d_18 \
 backbone/audio=avid_spec_cnn_9 \
@@ -12,7 +12,7 @@ environment.multiprocessing_distributed=True \
 environment.distributed=True \
 environment.ngpu=4 \
 environment.workers=32 \
-environment.data_dir=/home/logs/ \
+environment.data_dir=/home/data/ \
 logging.save_freq=10 \
 optim.epochs=100 \
 optim.args.lr=0.005 \
